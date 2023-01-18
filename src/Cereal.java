84687ee0062cf6d61204ee9e35b01deb05a2bbb2
HarwisarGheorghian <ggheorghian46@ladueschools.net>
2023-01-18 18:43:49 +0000

    initial commit

---
+++ src/Cereal.java
@@ -0,0 +1,139 @@
+public class Cereal{
+    private String name;
+    private char type;
+    private int calories;
+    private int protein;
+    private int fat;
+    private int sodium;
+    private int fiber;
+    private int carbohydrates;
+    private int sugar;
+    private int potassium;
+    private int vitamins;
+    private int shelf;
+    private int weight;
+    private double cups;
+    private double rating;
+    public Cereal(String name, char type, int calories, int protein, int fat, int sodium, int fiber, int carbohydrates, 
+    int sugar, int potassium, int vitamins, int shelf, int weight, double cups, double rating){
+        this.name = name;
+        this.type = type;
+        this.calories = calories;
+        this.protein = protein;
+        this.fat = fat;
+        this.sodium = sodium;
+        this.fiber = fiber;
+        this.carbohydrates = carbohydrates;
+        this.sugar = sugar;
+        this.potassium = potassium;
+        this.vitamins = vitamins;
+        this.shelf = shelf;
+        this.weight = weight;
+        this.cups = cups;
+        this.rating = rating; 
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return this.name;
+    }
+}
